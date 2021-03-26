0714bf5cf83672ad29d0d38e361595ae2924837a
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-26 10:43:12 -0700

    bubble page displaying correctly

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,10 +1,22 @@
+import Axios from "axios";
 import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth().get('/colors')
+      .then(res => {
+        setColorList(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="container">
